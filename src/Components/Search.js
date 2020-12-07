1c0acfc4e9d593d596fc5bee784d1c1b3c1c54cc
stasuravich <stasuravich@gmail.com>
2020-12-07 00:20:13 -0800

    search.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,15 +1,15 @@
-import React, {memo, useCallback} from 'react';
+import React, {memo} from 'react';
 import '../Css/Search.css';
 
 const Search = memo(props=>{
-  const loadQuery=useCallback(e=>{
+  const loadQuery=e=>{
     if(e.target.placeholder==="Search for a song..."){
       props.changeSearchRes(e.target.value);
     }
     else{
       props.setQuery(e.target.value);
     }
-  }, [props.changeSearchRes, props.setQuery]);
+  };
 
   return(
     <div className= {props.class} >
