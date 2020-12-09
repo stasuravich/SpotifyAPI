aa15b42ce59fae15bdda22cb02f933097cb4d427
stasuravich <stasuravich@gmail.com>
2020-12-09 15:01:50 -0800

    has tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,6 @@ import App from './App';
 import Login from './Components/Login'
 import {shallow, mount} from "enzyme";
 
-// const [cred, setCred]=useState("wrong");
-// const [qPlaylist, setQPlaylist]=useState("t");
-// const [loggedIn, setLoggedIn]=useState("wofubnoubnveo");
-
 describe("rendering components", ()=> {
   it("renders App without crashing", ()=>{
     shallow(<App/>);
@@ -15,8 +11,3 @@ describe("rendering components", ()=> {
     shallow(<Login/>);
   })
 })
-
-// describe("passing props", ()=>{
-//   const loginWrapper=mount(<Login setLoggedIn={setLoggedIn} cred={cred} setCred={setCred}/>);
-//   const searchWrapper=mount(<Search class="SearchPlaylist" placeh="Search playlist" setQuery={setQPlaylist}/>);
-// })
